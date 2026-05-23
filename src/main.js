02d5067410778ee0424a85b6a19fd365aade6dab
Nathanaël BUBBE <nathabu92@gmail.com>
2026-05-23 11:17:23 +0200

    6

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const { app, BrowserWindow, Menu, ipcMain, shell } = require("electron");
+const path = require("path");
+
+if (require("electron-squirrel-startup")) app.quit();
+
+let win;
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 1024,
+    minHeight: 640,
+    title: "Papillon",
+    icon: path.join(__dirname, "../assets/icon.png"),
+    backgroundColor: "#0f1a14",
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+    },
+  });
+
+  Menu.setApplicationMenu(null);
+  win.loadFile(path.join(__dirname, "index.html"));
+  win.on("closed", () => { win = null; });
+}
+
+app.whenReady().then(createWindow);
+app.on("window-all-closed", () => { if (process.platform !== "darwin") app.quit(); });
+app.on("activate", () => { if (!win) createWindow(); });
+
+ipcMain.on("open-external", (_, url) => shell.openExternal(url));
